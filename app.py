6f96a3949c8e61878f5a5144c87470b26d4c5b99
mehonal <116497904+mehonal@users.noreply.github.com>
2023-10-11 18:07:35 +0300

    added core functionality of converting pdf to html

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, request, render_template
 from pdf2image import convert_from_path
 import pytesseract
 
+PDF_PATH = "pdfs/"
+
 def convert_pdf_to_str(filename: str):
     try:
-        images = convert_from_path(filename)
+        images = convert_from_path(PDF_PATH + filename)
         full_text = ''
         for i, image in enumerate(images):
             text = pytesseract.image_to_string(image)
@@ -18,7 +20,7 @@ app: Flask = Flask(__name__)
 @app.route("/")
 def homepage():
     filename = request.args.get('filename')
-    if filename:
+    if filename is not None:
         output = convert_pdf_to_str(filename)
         if output != '':
             return output
